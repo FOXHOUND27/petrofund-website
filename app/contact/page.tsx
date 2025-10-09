2dc570a02d062410f5c3754a22528b9ef0bd5408
dalglish <dalglish45@gmail.com>
2025-10-09 21:00:38 +0200

    commiting current progress

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import MiniHero from "@/components/miniHero";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <MiniHero
+        imageSrc="/SectionImages/DesertHero.jpg"
+        title="Contact Us"
+        subtitle="Get in touch with us for any inquiries"
+      />
+    </>
+  );
+};
+
+export default Page;
