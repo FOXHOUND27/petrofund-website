a5daebe8d78e1a79cc6e1f12fc7d823d8da7e170
dalglish <dalglish45@gmail.com>
2025-10-15 22:12:57 +0200

    demo ready site

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,7 +1,16 @@
+import MainHero from "@/components/mainHero";
 import React from "react";
 
 const Page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <MainHero
+        imageSrc="/SectionImages/YellowDirtyLady.jpg"
+        title="Media"
+        subtitle="Our latest media"
+      />
+    </>
+  );
 };
 
 export default Page;
