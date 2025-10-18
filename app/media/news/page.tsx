59c9284edfa8e989b7b7ed83467058cffe5c914f
dalglish <dalglish45@gmail.com>
2025-10-18 13:44:24 +0200

    commiting current news changes

--- app/media/news/page.tsx
+++ app/media/news/page.tsx
@@ -57,7 +57,7 @@ const Page = () => {
         {posts.map((post) => (
           <div
             key={post.id}
-            className="p-6 sm:p-8 bg-[#4F3996] w-full sm:w-[90%] md:w-[350px] flex flex-col items-center rounded-tl-[45px] rounded-br-[45px] sm:rounded-tl-[55px] sm:rounded-br-[55px] h-auto sm:h-[500px] shadow-lg"
+            className="p-6 sm:p-8 shadow-xl bg-[#4F3996] w-full sm:w-[90%] md:w-[350px] flex flex-col items-center rounded-tl-[45px] rounded-br-[45px] sm:rounded-tl-[55px] sm:rounded-br-[55px] h-auto sm:h-[500px] shadow-lg"
           >
             <Image
               src={post.postImage}
